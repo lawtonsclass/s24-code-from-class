a5d4388a76ece8690deab54d6c2765262e6cc523
Lawton Nichols <lawtonnichols@gmail.com>
2024-04-09 16:33:59 -0700

    .

--- csci40-in-person/lec23/factorial.cpp
+++ csci40-in-person/lec23/factorial.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
 // returns n!
 int fact(int n) {
+  if (n < 1) {
+    throw runtime_error("Can't calulate the factorial of numbers < 1 :(");
+  }
+
   if (n == 1) {
     // base case: 1! = 1
     return 1;
@@ -13,8 +18,12 @@ int fact(int n) {
 }
 
 int main() {
-  cout << fact(5) << endl;
-  cout << fact(-1) << endl;
+  try {
+    cout << fact(5) << endl;
+    cout << fact(-1) << endl;
+  } catch (runtime_error& e) {
+    cout << e.what() << endl;
+  }
 
   return 0;
 }
