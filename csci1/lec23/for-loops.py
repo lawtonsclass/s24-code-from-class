16e956ac9fd33756079fe47c7efc521f5b050940
Lawton Nichols <lawtonnichols@gmail.com>
2024-04-10 17:12:26 -0700

    .

---
+++ csci1/lec23/for-loops.py
@@ -0,0 +1,20 @@
+L = [4, 5, 6]
+
+for x in L:
+    # x gets a chance to be every element in L, in order
+    print(x)
+
+print()
+
+for y in range(1, 11):
+    print(y)
+
+print()
+
+# iterate thorugh all the #s between 1 and 100
+# and add them into sum each time
+sum = 0
+for z in range(1, 101):
+    sum = sum + z
+
+print(sum)
